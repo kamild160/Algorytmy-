72b43b915e72a5154309f774953500f4f42fd0db
kamild160 <293206@uwr.edu.pl>
2019-03-12 21:18:01 +0100

    Update zad8.cpp

--- lista 1/zad8.cpp
+++ lista 1/zad8.cpp
@@ -1,23 +1,23 @@
 #include "ulamek.h"
 #include <iostream>
 
-//int main()
-//{
-//	ulamek a;
-//	ulamek b;
-//
-//	std::cin >> a;
-//	std::cin >> b;
-//
-//	ulamek plus = a + b;
-//	ulamek minus = a - b;
-//	ulamek mnoz = a * b;
-//	ulamek dziel = a / b;
-//
-//	std::cout << a << " + " << b << " = " << plus;
-//	std::cout << a << " - " << b << " = " << minus;
-//	std::cout << a << " * " << b << " = " << mnoz;
-//	std::cout << a << " / " << b << " = " << dziel;
-//
-//	getchar();
-//}
+int main()
+{
+	ulamek a;
+	ulamek b;
+
+	std::cin >> a;
+	std::cin >> b;
+
+	ulamek plus = a + b;
+	ulamek minus = a - b;
+	ulamek mnoz = a * b;
+	ulamek dziel = a / b;
+
+	std::cout << a << " + " << b << " = " << plus;
+	std::cout << a << " - " << b << " = " << minus;
+	std::cout << a << " * " << b << " = " << mnoz;
+	std::cout << a << " / " << b << " = " << dziel;
+
+	getchar();
+}
