55a50413f0daff7d8a70b94b5999badbe306d439
kamild160 <293206@uwr.edu.pl>
2019-04-02 21:44:33 +0200

    dobre 16

--- lista 2/zad16.cpp
+++ lista 2/zad16.cpp
@@ -13,17 +13,14 @@ struct node{
 class iterator
 {
 private:
-	node* currentNode; //wskaźnik na obecny węzeł 
+	node* currentNode; // wskaznik na obecny wezel
 
 public:
-//konstruktor 
-	iterator(node* t) :  
-		currentNode(t)
-	{
-	}
-	
-//wypisuje elementy w kolejności wpisywania 
-	iterator begin(node* t)
+	iterator(node* t) :
+		currentNode(t) //przypisanie node do currentNode
+	{ }
+//przechodzi przez elementy w kolejnosci inorder
+	iterator begin(node* t) //idzie od maxa z gory w lewo 
 	{
 		if(t)
 		{
@@ -31,69 +28,66 @@ public:
 			{
 				t = t->left;
 			}
-		} 
-		return iterator(t); //wypisuje ostatni 
+		}
+		return iterator(t); //zwraca obiekt klasy iterator ostatni po lewej
 	}
-	
-	iterator end(node* t)
+
+	iterator end(node* t) //jeeli dojdzie do kona
 	{
-		return iterator(nullptr);
+		return iterator(nullptr);  //zwraca ostatni element
 	}
-	
-	iterator& operator = (node* t)
+
+	iterator& operator = (node* t) //przeciąanie operatora przypisania, zeby for działał na node 
 	{
-		this->currentNode = t;//przekazujemy zapisany element 
+		this->currentNode = t; //zapisujemy emelent otrztmany ew argumencie, podobne wykananie jak w konstruktorze 
 		return *this;
 	}
-	
-	iterator operator ++ (int)
+
+	iterator operator ++ (int)//przeciązanie operatora, przechodzi do następnego więzła
 	{
-		currentNode = nextNode(this->currentNode);//przechodzimy na następny element 
+		currentNode = nextNode(this->currentNode); // przejscie do nastepnego  elementu
 		return currentNode;
 	}
-	
-	bool operator != (const iterator& it)
+
+	bool operator != (const iterator& it) // kontynłuacji pentli , const zeby się nie modyfikował 
 	{
-		return currentNode != it.currentNode; //sprawdza czy nasz currentnode nie jest rowny otrzymanemu currentnode danego obiektu 
+		return currentNode != it.currentNode; // sprawdza czy nasz currnode nie jest rowny otrzymanemu currentnodowi danego obiektu.
 	}
-	
+
 	int operator* ()
 	{
-		return currentNode->x; //zwraca klucz z currentnode
+		return currentNode->x; // zwraca klucz z current node
 	}
-	
-	node* nextNode(node* t)
+
+	node* nextNode(node* t) //przejście do następnego elementu inorder - do pętli for 
 	{
-		node* t1 = t;
-		if(t->right == nullptr)
+		node* t1 = t; //tworzymy zmienna pomocnicza t1 do ktorej zapisujemy node'a przekazanego w agrumencie.
+		if(t->right == nullptr) //dopoki right child node'a "t" jest nullem.
 		{
-			while(t->parent != nullptr && t1->x > t->parent->x)
+			while(t->parent != nullptr && t1->x > t->parent->x)//dopoki parent node'a "t" nie jest nullem i klucz node'a "t1" jest wiekszy od klucza parenta node'a "t".
 			{
-				
-				t = t->parent;
+				t = t->parent;//Przejdz do nastepnego parenta w nodzie "t"
 			}
-			return t->parent;
+			return t->parent;// po wyjsciu z petli wihile zwroc ostatniego zapisanego parenta node'a "t"
 		}
-		else
+		else // jesli right child nie jest nullem.
 		{
-			t = t->right;
-			while (t->left != nullptr && t->left->x > t1->x)
+			t = t->right; //przejdz raz do right child'a node'a "t" , przejdź na maxa w lewo
+			while (t->left != nullptr && t->left->x > t1->x)//dopoki left child node'a "t" nie jest nullem i klucz left child'a jest ,przejdź na wiekszy od  klucza node'a "t1".
 			{
-				t = t->left;
+				t = t->left; // Przejdz do nastepnego left child'a w nodzie "t"
 			}
-		return t;
+		return t;// zwroc node'a "t" z ostatnim zapisanym left child'em z petli while.
 		}
 	}
 };
 };
-
-
 
 void insert(node*& t, int x)
 {
 	node** current = &t;
 	node** parent = nullptr;
-	
+
 	while(*current)
 	{
 		parent = current;
@@ -106,7 +100,7 @@ void insert(node*& t, int x)
 			current = &((*current)->right);
 		}
 	}
-	
+
 	if(parent != nullptr)
 	{
 		*current = new node(x, *parent);
@@ -118,30 +112,29 @@ void insert(node*& t, int x)
 
 }
 
-
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
-    node* myNode = nullptr;
-	
-	insert(myNode, 15);
-	insert(myNode, 6); 
-	insert(myNode, 2); 
-	insert(myNode, 10);
-	insert(myNode, 8);
-	insert(myNode, 7);
-	insert(myNode, 9);
-	insert(myNode, 12);
-	insert(myNode, 17);
-	insert(myNode, 20);
-	
-	
-	for(node::iterator it = it.begin(myNode); it != it.end(myNode); it++)
+
+    node* L = nullptr;
+
+	insert(L, 15);
+	insert(L, 6);
+	insert(L, 2);
+	insert(L, 10);
+	insert(L, 8);
+	insert(L, 7);
+	insert(L, 9);
+	insert(L, 12);
+	insert(L, 17);
+	insert(L, 20);
+
+
+	for(node::iterator it = it.begin(L); it != it.end(L); it++) //w node towrzę obiekt iterator , idę na maxa w lewo w elemencie; jeli obecny it nie jest ostenim; przejdź do następnego| w porządku inorder
 	{
 		std::cout << *it << std::endl;
 	}
-
     return 0;
+
 }
