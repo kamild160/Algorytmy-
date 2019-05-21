35632078aef06c2e6fd3d26781ae7c48d1ef0085
kamild160 <293206@uwr.edu.pl>
2019-05-21 22:04:03 +0200

    Add files via upload

--- lista 7/zad6.cpp
+++ lista 7/zad6.cpp
@@ -1,66 +1,95 @@
-    bool DoesPathExistA(const size_t& n,const bool t[][5])
+#include <iostream>
+
+
+    class UnionFind
     {
-        if(!t[0][0])
-            return 0;
-        UnionFind obj(n*n); 
-        for(size_t i = 0; i < n; ++i)
-            for(size_t j = 0; j < n; ++j)
+        public:
+            UnionFind(int n) : m_parent{new int[n]}, m_rank{new int[n]}
             {
-                if(t[i][j])
+                for(int i = 0; i < n; ++i)
                 {
-                    if(i+1 < n && t[i+1][j]) //right
-                        obj.make_union(i*n+j,(i+1)*n+j);
-                    if(j+1 < n && t[i][j+1]) //down
-                        obj.make_union(i*n+j,i*n+j+1);
-                 }
+                    m_parent[i] = i;
+                    m_rank[i] = 0;
+                }
             }
-        return obj.find(0) == obj.find(n*n-1) ? 1 : 0;
-    }
-    bool DoesPathExistB(const size_t& n,const bool t[][5])
-    {
-        UnionFind obj(n*n); 
-        for(size_t i = 0; i < n; ++i)
-            for(size_t j = 0; j < n; ++j)
+            int find(int i)
             {
-                if(t[i][j])
+                if(m_parent[i] == i)
+                    return i;
+                return m_parent[i] = find(m_parent[i]);
+            }
+            void make_union(int i,int j)
+            {
+                int a = find(i);
+                int b = find(j); 
+                if(a!=b)
+                    if(m_rank[a] < m_rank[b])
+                        m_parent[a] = b;
+                    else if(m_rank[b] < m_rank[a])
+                        m_parent[b] = a;
+                    else
+                    {
+                        m_parent[a] = b;
+                        ++m_rank[b];
+                    }
+            }
+            ~UnionFind()
+            {
+                delete[] m_rank;
+                delete[] m_parent;
+            }
+        private:
+            int* m_parent;
+            int* m_rank;
+    };
+    
+    bool DoesPathExist(int n,const bool t[][5])
+    {
+        UnionFind obj(n*n); // tworzymy obiekt z ilością pól równą N*N.
+        for(int i = 0; i < n; ++i) // pętla dla kolumn
+            for(int j = 0; j < n; ++j) // pętla dla wierszy.
+            {
+                if(t[i][j]) // jesli pole istnieje 
                 {
-                    if(i+1 < n && t[i+1][j]) //right
+                    if(i+1 < n && t[i+1][j]) //down //  if-y sprawdzają czy w obrębie 1 są inne jedynki jesli tak scala je w wyspę.
                         obj.make_union(i*n+j,(i+1)*n+j);
-                    if(i-1 >= 0 && t[i-1][j]) //left
+                    if(i-1 >= 0 && t[i-1][j]) //up
                         obj.make_union(i*n+j,(i-1)*n+j);
-                    if(j+1 < n && t[i][j+1]) //down
+                    if(j+1 < n && t[i][j+1]) //right
                         obj.make_union(i*n+j,i*n+j+1);
                  }
             }
 
-        for(size_t i = 0; i < n; ++i)
-            if(t[0][i])
-                for(size_t j = 0; j < n; ++j)
-                    if(t[n-1][j])
-                        if(obj.find(i) == obj.find((n-1)*n+j))
+        for(int i = 0; i < n; ++i)
+            if(t[i][0]) // jeśli istnieje taki el
+                for(int j = 0; j < n; ++j) // pętla po wierszach
+                    if(t[j][n-1]) // jesli istenie taki element
+                        if(obj.find(i*(n-1)) == obj.find(j*(n-1)+(n-1))) // sprawdza czy ścieżka istnieje jesli tak zwraca 1 (jeśli poczatkowy punkt ma tego samego reprezentanta co końcowy to zwróc 1 )
                             return 1;
         return 0;
     }
-}
+
 int main()
 {
-    constexpr size_t n = 5;
-    constexpr bool t[n][n] = {{1,1,0,0,0},
+     const int n = 5;
+        const bool t1[n][n] ={{1,1,1,1,1},
                               {0,1,0,0,1},
-                              {0,0,1,0,0},
-                              {0,0,0,0,0},
+                              {0,0,1,0,1},
+                              {0,0,0,0,1},
                               {1,0,1,0,1}};
 
-    constexpr bool t2[n][n] = {{1,1,0,0,0},
+        const bool t2[n][n] = {{1,1,0,0,0},
                                {1,0,0,0,1},
                                {1,1,0,0,0},
                                {0,1,0,1,0},
                                {1,1,1,1,1}};
 
-    constexpr bool t3[n][n] = {{0,1,1,0,0},
+        const bool t3[n][n] = {{0,1,1,0,0},
                                {0,0,1,0,1},
                                {0,1,1,0,0},
-                               {0,1,1,1,0},
-                               {1,0,1,1,1}};
-    std::cout << ASD::DoesPathExistB(n,t3) << std::endl;
+                               {0,0,1,1,0},
+                               {1,1,0,1,1}};
+    std::cout << DoesPathExist(n,t1) << std::endl;
+    std::cout << DoesPathExist(n,t2) << std::endl;
+    std::cout << DoesPathExist(n,t3) << std::endl;
 }
--- lista 7/zad7.cpp
+++ lista 7/zad7.cpp
@@ -1,11 +1,10 @@
-
-    size_t countIslands(const size_t& n, const bool a[][5])
+int countIslands(int n, const bool a[][5])
     {
-        UnionFind obj(n*n);
-        for(size_t i = 0; i < n; ++i)
-            for(size_t j = 0; j < n; ++j)
+        UnionFind obj(n*n); // tworzymy obiekt z ilością pól równą N*N (w naszym przypadku 25).
+        for(int i = 0; i < n; ++i) // pętla dla kolumn
+            for(int j = 0; j < n; ++j) // pętla dla wierszy.
             {
-                if(!a[i][j])
+                if(!a[i][j]) // jeli nie ma takiego elementu. // if-y sprawdzają czy w obrębie 1 są inne jedynki jesli tak scala je w wyspę.
                     continue;
                 if(i+1 < n && a[i+1][j])
                     obj.make_union(i*n+j,(i+1)*n+j);
@@ -16,27 +15,30 @@
                 if(j+1 < n && a[i][j+1])
                     obj.make_union(i*n+j,i*n+j+1); 
             }
-        bool temp[n] = {0};
-        size_t numberOfIslands = 0;
-        for(size_t i = 0; i < n; ++i)
-            for(size_t j = 0; j < n; ++j)
-                if(a[i][j])
+        bool temp[n*n] = {0}; // kontener przetrzymujący zliczone wyspe
+        int numberOfIslands = 0; // licznik wysp.
+        for(int i = 0; i < n; ++i) // petla po kolumnach
+            for(int j = 0; j < n; ++j) //pętla po wierszach
+                if(a[i][j]) 
                 {
-                    size_t x = obj.find(i*n+j);
-                    if(!temp[x]++)
-                        ++numberOfIslands;
+                    int x = obj.find(i*n+j); // znajdz korzeń i zrób korzeń parentem i  // reprezentant 
+                    if(!temp[x]) // jeśli wyspy nie ma w kontenerze temp
+                    {
+                        ++numberOfIslands; // zwieksz licznik
+                        temp[x] = 1; // dodaj do reprezentanta do  kontenera
+                    }
+                        
                 }  
         return numberOfIslands;
     }
-}
+
 int main()
 {
-    /* 0 - sea, 1 - island */
-    constexpr size_t n = 5;
+    const int n = 5;
     bool t[n][n] = {{1,1,0,0,0},
-                    {0,1,0,0,1},
+                    {0,1,0,0,0},
                     {0,0,1,0,0},
-                    {0,0,0,0,0},
+                    {0,0,0,1,0},
                     {1,0,1,0,1}};
-    std::cout << ASD::countIslands(n,t) << std::endl;
+    std::cout << countIslands(n,t) << std::endl;
 }
